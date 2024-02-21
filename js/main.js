75f78876dfe1b00ad3fb21df8a19118287b9f477
Joodarbeshim <anonymous_face@icloud.com>
2024-02-21 11:26:20 +0600

    init

---
+++ js/main.js
@@ -0,0 +1,24 @@
+// const currentColor = localStorage.getItem("bgcolor");
+
+// if (!currentColor) {
+//   localStorage.setItem("bgcolor", "tomato");
+// }
+
+// // localStorage.removeItem("bgcolor");
+// const input = document.querySelector('input[type="text"]');
+
+// function init() {
+//   const bgcolor = localStorage.getItem("bgcolor");
+//   if (bgcolor) {
+//     setBodyColor(bgcolor);
+//     input.value = bgcolor;
+//   }
+// }
+
+// function setBodyColor(color) {
+//   document.body.style.background = color;
+// }
+
+// init.adde;
+
+// init();
